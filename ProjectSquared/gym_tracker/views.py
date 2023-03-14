b4765239761c5a9e5d9cbd6cf839f9e3ee195a39
IsaacBosman <isaacbosman@gmail.com>
2023-03-14 17:14:41 +0000

    Added form

--- ProjectSquared/gym_tracker/views.py
+++ ProjectSquared/gym_tracker/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .forms import SpSheetForm
-
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index")
@@ -11,4 +10,8 @@ def index(request):
     return render(request, 'index.html')
 
 def weights_page(request):
-    return render(request, 'weights_table.html')
+    if request.method == 'POST':
+        form = SpSheetForm(request.POST, request.FILES)
+    else:
+        form = SpSheetForm()
+    return render(request, 'weights_table.html', {'form':form})
